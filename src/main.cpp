326fe1a826661df7e76a7e13067a11f61bf22a0a
71frukt <71frukt@mail.ru>
2024-10-17 01:29:25 +0300

    logs only on debug

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,15 @@
 #include "spu_debug.h"
 
 const char  *code_file_name = "txts/program_code.txt";
-const char  *logs_file_name = "txts/spu_logs.txt";
-int REGISTERS[REGISTERS_NUM] = {};
+const char  *logs_file_name = "txts/logs/spu_logs.txt";
 
 int main()
 {
+    int REGISTERS[REGISTERS_NUM] = {};
     FILE *code_file = fopen(code_file_name, "r");
-    FILE *log_file = fopen(logs_file_name, "w");
+    ON_DEBUG(FILE *log_file = fopen(logs_file_name, "w"));
 
-    cmd_t cmd = {.pi = 0};
+    cmd_t cmd = {.ip = 0}; // TODO: ip
 
     for (cmd.size = 0; cmd.size < MAX_CMD_SIZE; cmd.size++)
     {
@@ -27,25 +27,25 @@ int main()
 
     bool keep_doing = true;
 
-    while (cmd.pi < cmd.size && keep_doing)
+    while (cmd.ip < cmd.size && keep_doing)
     {
-        switch (cmd.code[cmd.pi])
+        switch (cmd.code[cmd.ip])
         {
         case PUSH:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
-            StackElem_t arg = cmd.code[++cmd.pi];
+            StackElem_t arg = cmd.code[++cmd.ip];
 
             StackPush(stk, arg);
-            cmd.pi++;
+            cmd.ip++;
             
             break;
         }
 
         case ADD:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
             StackElem_t a = 0;
             StackElem_t b = 0; 
@@ -54,14 +54,14 @@ int main()
             StackPop(stk, &b);
 
             StackPush(stk, a + b); 
-            cmd.pi++;
+            cmd.ip++;
             
             break;
         }
 
         case MUL:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
             StackElem_t a = 0;
             StackElem_t b = 0;
@@ -70,14 +70,14 @@ int main()
             StackPop(stk, &b);
 
             StackPush(stk, a * b);
-            cmd.pi++;
+            cmd.ip++;
 
             break;
         }
 
         case DIV:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
             StackElem_t divisible = 0;
             StackElem_t splitter  = 0;
@@ -86,62 +86,62 @@ int main()
             StackPop(stk, &splitter);
 
             StackPush(stk, divisible / splitter);
-            cmd.pi++;
+            cmd.ip++;
 
             break;
         }
 
         case OUT:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
             StackElem_t res = 0;
             StackPop(stk, &res);
             fprintf(stderr, "res = %d\n", res);
-            cmd.pi++;
+            cmd.ip++;
             
             break;
         }
 
         case PUSHR:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
-            StackElem_t arg = cmd.code[++cmd.pi];
+            StackElem_t arg = cmd.code[++cmd.ip];
 
             StackPush(stk, REGISTERS[arg]);
-            cmd.pi++;
+            cmd.ip++;
             
             break;
         }
 
         case POPR:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
-            StackElem_t arg = cmd.code[++cmd.pi];
+            StackElem_t arg = cmd.code[++cmd.ip];
 
             StackPop(stk, &REGISTERS[arg]);
-            cmd.pi++;
+            cmd.ip++;
             
             break;
         }
 
         case JUMP:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
-            StackElem_t arg = cmd.code[++cmd.pi];
-            cmd.pi = arg;
+            StackElem_t arg = cmd.code[++cmd.ip];
+            cmd.ip = arg;
             
             break;     
         }
 
         case HLT:
         {
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
             
-            cmd.pi++;
+            cmd.ip++;
 
             keep_doing = false;
             
@@ -150,17 +150,17 @@ int main()
 
         default:
         {
-            fprintf(code_file, "Syntax error: '%llu'\n", cmd.code[cmd.pi]);
+            fprintf(code_file, "Syntax error: '%llu'\n", cmd.code[cmd.ip]);
             
-            ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+            ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
             break;
         }
         }
     }
     
-    ON_DEBUG(SPU_DUMP(REGISTERS, &cmd, log_file));
+    ON_DEBUG(SPU_DUMP(log_file, REGISTERS, &cmd));
 
-    fclose(log_file);
+    ON_DEBUG(fclose(log_file));
     fclose(code_file);
 
     return 0;
