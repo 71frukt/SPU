f6b28848625c5ba46cad4e104dfa5e35f30c1d08
71frukt <71frukt@mail.ru>
2024-10-16 15:07:11 +0300

    add struct comand_t and trans_comands_t

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -2,52 +2,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-const int MAX_FUNC_NAME = 20;
+const int COMAND_NAME_LEN = 20;
 const char *trans_file_name = "txts/translator.txt";
 const char *asm_file_name   = "txts/program.asm";
 const char *code_file_name  = "txts/program_code.txt";
 
 size_t GetCountOfLines(FILE *text);
 
+typedef struct Comand
+{
+    char name[COMAND_NAME_LEN];
+    int code;
+} comand_t;
+
+typedef struct TranstationComands
+{
+    comand_t *comands;
+    size_t size;
+} trans_comands_t;
+
+void GetComands(const char *file_name, trans_comands_t *trans_comands);
+
 int main()
 {
-    FILE *trans_file = fopen(trans_file_name, "r");
-
-    size_t num_trans_lines = GetCountOfLines(trans_file);
-
-    char *func_names = (char *) calloc(num_trans_lines * MAX_FUNC_NAME, sizeof(char));
-    int  *func_codes = (int *)  calloc(num_trans_lines, sizeof(int));
-
-    for (size_t i = 0; i < num_trans_lines; i++)
-    {
-        fscanf(trans_file, "%s", func_names + i * MAX_FUNC_NAME);
-        fscanf(trans_file, "%d", func_codes + i);
-    }
-
-    fclose(trans_file);
+    trans_comands_t trans_comands = {};
+    GetComands(trans_file_name, &trans_comands);
 
     FILE *asm_file  = fopen(asm_file_name,  "r");    
     FILE *code_file = fopen(code_file_name, "w");
 
-    char cur_func_name[MAX_FUNC_NAME] = {};
+    char cur_comand_name[COMAND_NAME_LEN] = {};
 
-    while (fscanf(asm_file, "%s", cur_func_name) == 1)
+    while (fscanf(asm_file, "%s", cur_comand_name) == 1)
     {
-        for (size_t i = 0; i < num_trans_lines; i++)
+        bool code_is_founded = false;
+
+        for (size_t i = 0; i < trans_comands.size; i++)
         {
-            if (strcmp(func_names + i * MAX_FUNC_NAME, cur_func_name) == 0)
+            if (strcmp(trans_comands.comands[i].name, cur_comand_name) == 0)
             {
-                fprintf(code_file, "%d ", func_codes[i]);
-                if (strcmp(cur_func_name, "push") == 0)
+                fprintf(code_file, "%d ", trans_comands.comands[i].code);
+
+                if (strcmp(cur_comand_name, "push") == 0)
                 {
                     int elem = 0;
                     fscanf(asm_file, "%d", &elem);
                     fprintf(code_file, "%d ", elem);
                 }   
 
+                code_is_founded = true;
                 break;
-            }
+            }     
         }
+
+        if (!code_is_founded)
+            fprintf(stderr, "Unknown comand: '%s'\n", cur_comand_name);
     }
     
     fclose(asm_file);
@@ -69,4 +78,25 @@ size_t GetCountOfLines(FILE *text)
     
     fseek(text, 0, SEEK_SET);
     return num_lines;
+}
+
+void GetComands(const char *file_name, trans_comands_t *trans_comands)
+{
+    FILE *trans_file = fopen(file_name, "r");
+
+    size_t num_trans_lines = GetCountOfLines(trans_file);
+
+    trans_comands->size = num_trans_lines;
+
+    comand_t *comands_tmp = (comand_t *) calloc(num_trans_lines, sizeof(comand_t));
+
+    for (size_t i = 0; i < num_trans_lines; i++)
+    {
+        fscanf(trans_file, "%s", comands_tmp[i].name);
+        fscanf(trans_file, "%d", &comands_tmp[i].code);
+    }
+
+    trans_comands->comands = comands_tmp;
+
+    fclose(trans_file);
 }
